da670ed10c043b5f9b7d6a665eb656b3184485e2
Roy Velich <6681067+royvelich@users.noreply.github.com>
2026-05-05 09:25:51 +0300

    Update train.py

--- train.py
+++ train.py
@@ -38,12 +38,12 @@ def main(cfg: DictConfig) -> None:
     torch.manual_seed(cfg.globals.training_seed)  # seeds CPU + all CUDA devices
     torch.cuda.manual_seed_all(cfg.globals.training_seed)  # explicit, in case you fork
 
-    # Force deterministic algorithms; raises if a non-deterministic op is used
-    torch.use_deterministic_algorithms(True, warn_only=False)
-
-    # cuDNN: disable autotuner + force deterministic conv kernels
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
+    # # Force deterministic algorithms; raises if a non-deterministic op is used
+    # torch.use_deterministic_algorithms(True, warn_only=False)
+    #
+    # # cuDNN: disable autotuner + force deterministic conv kernels
+    # torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True
 
 
 
